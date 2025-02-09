427e239a12e1fca44ba12307070a22ddabf1f0c9
serhii <ockar2018@gmail.com>
2025-02-09 21:18:50 +0200

    add func to validate city name

--- src/tasks.py
+++ src/tasks.py
@@ -1,29 +1,17 @@
+from typing import List
+
+import orjson
+from redis.asyncio import Redis
+from src.services.file_writer import save_to_file
 from src.services.weather_api import fetch_weather
-from src.services.file_writer import save_weather_data
-import asyncio
-import logging
-
-logging.basicConfig(level=logging.INFO)
 
 
-async def process_city(city: str, results: dict):
-    try:
-        weather_data = await fetch_weather(city)
-        if weather_data:
-            results[city] = weather_data
-        else:
-            logging.warning(f"No weather data for {city}")
-    except Exception as e:
-        logging.error(f"Error processing city {city}: {e}")
-
-
-async def process_weather_data(cities: list):
+async def process_weather_data(cities: List[str], task_id: str, redis_client: Redis):
     results = {}
-    tasks = [process_city(city, results) for city in cities]
-    await asyncio.gather(*tasks)
+    for city in cities:
+        weather_data = await fetch_weather(city)
+        results[city] = weather_data
 
-    if not results:
-        logging.warning("No valid weather data received for any city.")
-
-    await save_weather_data("global", results)
+    data = orjson.loads(await redis_client.get(task_id) or "{}")
+    await save_to_file(task_id, data)
     return results
