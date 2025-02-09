71c833687651e9aa083ad9e512cf1f834edfcdae
serhii <ockar2018@gmail.com>
2025-02-09 21:14:53 +0200

    fix imports and add logic for endpoints

--- api/web/routers/weather.py
+++ api/web/routers/weather.py
@@ -1,10 +1,12 @@
-import orjson
 import uuid
-
-from fastapi import APIRouter, HTTPException
+import orjson
+from fastapi import APIRouter, Depends, HTTPException
+from redis.asyncio import Redis
 
 from api.web.schemas.weather import WeatherRequestSchema
-# from src.main import redis_client
+from src.redis_tools.tools import get_redis
+from src.services.data_cleaner import clean_city_name
+from src.services.file_writer import save_to_file, get_file_path
 from src.services.weather_api import transform_weather_response
 from src.tasks import process_weather_data
 
@@ -12,24 +14,38 @@ weather_router = APIRouter()
 
 
 @weather_router.post("/weather")
-async def get_weather(weather_request: WeatherRequestSchema):
-    cities = weather_request.cities
+async def get_weather(request: WeatherRequestSchema, redis_client: Redis = Depends(get_redis)):
     task_id = str(uuid.uuid4())
-    # await redis_client.set(task_id, orjson.dumps({"status": "running"}).decode())
+    await redis_client.set(task_id, orjson.dumps({"status": "running"}).decode())
 
-    results = await process_weather_data(cities)
-    transformed_results = ({
-        "status": "completed",
-        "results": transform_weather_response(results.values())
-    })
+    cities = [await clean_city_name(city) for city in request.cities]
+    results = await process_weather_data(cities, task_id, redis_client)
 
-    # await redis_client.set(task_id, orjson.dumps(transformed_results).decode())
+    transformed_results = {
+        "status": "completed" if results else "failed",
+        "results": transform_weather_response(results.values()).results
+    }
+
+    if results:
+        file_path = get_file_path(task_id, transformed_results["results"])
+        await save_to_file(task_id, transformed_results)
+        transformed_results["file_url"] = file_path
+
+    await redis_client.set(task_id, orjson.dumps(transformed_results).decode())
     return transformed_results
 
 
-# @weather_router.get("/tasks/{task_id}")
-# async def get_task_status(task_id: str):
-#     task_data = await redis_client.get(task_id)
-#     if not task_data:
-#         raise HTTPException(status_code=404, detail="Task ID not found")
-#     return orjson.loads(task_data).encode()
+@weather_router.get("/tasks/{task_id}")
+async def get_task_status(task_id: str, redis_client: Redis = Depends(get_redis)):
+    task_data = await redis_client.get(task_id)
+    if not task_data:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Task with ID '{task_id}' not found"
+        )
+
+    task_info = orjson.loads(task_data)
+    response = {
+        "status": task_info["status"],
+    }
+    return response
